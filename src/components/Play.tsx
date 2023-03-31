afe70acb49edbbe32a13eff3297dbad0d3dabe6c
JimmyGx6 <jaime92_92@hotmail.com>
2023-03-31 20:06:42 +0200

    Función generadora de tirada y muestra de resultados por pantalla

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,39 +1,56 @@
 import { IonButton } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Play = () => {
+
+  const [player, setPlayer] = useState("");
+  const [computer, setComputer] = useState("");
 
   const choices = [
     {
       name: "piedra",
       beats: "tijeras",
-      loses: "papel"
+      loses: "papel",
     },
     {
       name: "papel",
       beats: "piedra",
-      loses: "tijeras"
+      loses: "tijeras",
     },
     {
       name: "tijeras",
       beats: "papel",
-      loses: "piedra"
-    }
+      loses: "piedra",
+    },
   ];
 
   const randomChoice = () => {
     const randomNumber = Math.floor(Math.random() * choices.length);
     const playerChoice = choices[randomNumber].name;
     return playerChoice;
-  }
+  };
 
+  
+  const newTurn = () => {
+    const player = setPlayer(randomChoice());
+    const computer = setComputer(randomChoice());
+    const roll = [];
+    roll.push(player, computer);
+    return roll;
+  };
+
+ const winner = () => {
+
+}
 
   const handleClick = () => {
-    console.log(randomChoice());
+    return newTurn();
   };
 
   return (
     <>
+      <h2> {player} </h2>
+      <h2> {computer} </h2>
       <IonButton onClick={handleClick}> Jugar </IonButton>
     </>
   );
