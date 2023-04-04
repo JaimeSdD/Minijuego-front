b9d8e102d501a171555b5ee92c663073975d6f48
JimmyGx6 <jaime92_92@hotmail.com>
2023-04-04 15:35:10 +0200

    Ajuste por la llevada de la lógica al back y nuevas funciones para conectar con la api: mandar tirada y recibir el historial

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -3,68 +3,68 @@ import React, { useState } from "react";
 import { API } from "../services/api";
 
 const Play = () => {
-  const [playerSelection, setPlayer] = useState("");
-  const [computerSelection, setComputer] = useState("");
-  const [wins, setWins] = useState(0);
-  const [losses, setLosses] = useState(0);
-  const [ties, setTies] = useState(0);
-  const [result, setResult] = useState("");
+  // const [playerSelection, setPlayer] = useState("");
+  // const [computerSelection, setComputer] = useState("");
+  // const [wins, setWins] = useState(0);
+  // const [losses, setLosses] = useState(0);
+  // const [ties, setTies] = useState(0);
+  // const [result, setResult] = useState("");
 
   const choices = [
     {
       name: "piedra",
-      beats: "tijeras",
-      loses: "papel",
     },
     {
       name: "papel",
-      beats: "piedra",
-      loses: "tijeras",
     },
     {
       name: "tijeras",
-      beats: "papel",
-      loses: "piedra",
     },
   ];
 
+  let historical:any[] = [];
+
   const randomChoice = () => {
     const randomNumber = Math.floor(Math.random() * choices.length);
-    return choices[randomNumber];
+    return choices[randomNumber].name;
   };
 
-  const newTurn = () =>  [randomChoice(), randomChoice()];
+  const sendHistorical = async () => {
 
-  const computeResult = () => {
-    const [player, computer] = newTurn();
-    setPlayer(player.name);
-    setComputer(computer.name);
+    const player = randomChoice();
 
-    if (player.beats.includes(computer.name)) {
-      setWins(wins + 1);
-      setResult("Victoria");
-    } else if (player.loses.includes(computer.name)) {
-      setLosses(losses + 1);
-      setResult("Derrota");
-    } else {
-      setTies(ties + 1);
-      setResult("Empate");
-    }
+    historical.push({player});
+    JSON.stringify(historical);
+
+    const result = await API.post("/player", {historical});
+    console.log(result.config.data);
+
+    getHistorical();
   };
 
-  const handleClick = async () => {
-    const result = await API.get("/test1");
-    return console.log(result);
+  const getHistorical = async () => {
+    const result = await API.get("/historical");
+    historical = result.data.historical;
+
+    console.log(historical);
+  }
+
+
+  const handleClick = () => {
+
+    sendHistorical();
+    // getHistorical();
+
   };
 
   return (
     <>
-      <h2> {playerSelection} </h2>
+      {/* <h2> {playerSelection} </h2>
       <h2> {computerSelection} </h2>
       <h2> {result} </h2>
       <h3> {wins} </h3>
       <h3> {losses} </h3>
-      <h3> {ties} </h3>
+      <h3> {ties} </h3> */}
       <IonButton onClick={handleClick}> Jugar </IonButton>
     </>
   );
