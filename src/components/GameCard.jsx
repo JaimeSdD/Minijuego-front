82ad442d417aabdbbf57e1f9fba2f5286a23a872
JimmyGx6 <jaime92_92@hotmail.com>
2023-04-10 19:09:24 +0200

    tirada de la máquina en pantalla

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,4 +1,4 @@
-import { IonCard } from "@ionic/react";
+import { IonCard, IonCardTitle } from "@ionic/react";
 import { useEffect, useState } from "react";
 import "../styles/GameCard.css"
 
@@ -7,23 +7,33 @@ const GameCard = ({ historical }) => {
     piedra: "./assets/rock.png",
     tijeras: "./assets/scissors.png",
     papel: "./assets/paper.png",
-    playerDefault: "./assets/chuck.jpg"
+    playerDefault: "./assets/chuck.jpg",
+    computerDefault: "./assets/terminator.jpg"
   };
   const newTurn = historical[historical.length - 1];
 
   const [playerImage, setPlayerImage] = useState(images[newTurn?.player] || images.playerDefault);
+  const [computerImage, setComputerImage] = useState(images[newTurn?.computer] || images.computerDefault);
 
   useEffect(() => {
-    setPlayerImage(images[newTurn?.player] || images.playerDefault)
+    setPlayerImage(images[newTurn?.player] || images.playerDefault);
+    setComputerImage(images[newTurn?.computer] || images.computerDefault);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newTurn?.player])
+  }, [newTurn])
 
 
   return (
     <div className="image-container">
       <IonCard>
+      <IonCardTitle>Jugador: {newTurn?.player}</IonCardTitle>
       <div className="image-wrapper">
         <img src={playerImage} alt={newTurn?.player}/>
+      </div>
+      </IonCard>
+      <IonCard>
+      <IonCardTitle>La máquina: {newTurn?.computer}</IonCardTitle>
+      <div className="image-wrapper">
+        <img src={computerImage} alt={newTurn?.computer}/>
       </div>
       </IonCard>
     </div>
